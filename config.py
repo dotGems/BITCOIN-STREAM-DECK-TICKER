6d483c5c607959ae3dba3adcfaec5af41c761d62
daverex956 <107266909+daverex956@users.noreply.github.com>
2023-11-10 12:21:57 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ sdFolderDir = "FILE_DIRECTORY_HERE"
 
 # dir to images
 ## Digit 1 - $
-d1 = "TTQM3OKFAP77L4VT0JVFMPVR5SZ.png" #example TTQM3OKFAP77L4VT0JVFMPVR5SZ.png
+d1 = "FILE_NAME_HERE" #example TTQM3OKFAP77L4VT0JVFMPVR5SZ.png
 ## Digit 2 - 3
-d2 = "SH1LFQM1TD6E73IEDKQD38FH9CZ.png"
+d2 = "FILE_NAME_HERE"
 ## Digit 3 - 5
-d3 = "PC4LAN6J0T04BA1BCGHTNEVLDKZ.png"
+d3 = "FILE_NAME_HERE"
 ## Digit 4 - 3
-d4 = "A9AV0TB4SH2HW5SVWJVIP31AP0Z.png"
+d4 = "FILE_NAME_HERE"
 ## Digit 5 - 6
-d5 = "0NWSRVBHL95OW1QT1A8NDJJKFGZ.png"
+d5 = "FILE_NAME_HERE"
 ## Digit 6 - 2
-d6 = "FDT33001LL0DF5E84IMFCQJL8KZ.png"
+d6 = "FILE_NAME_HERE"
 
 # STEP 3 : open terminal, type "cd" then a space ... then drag and drop the folder where this .py file is stored. (this points terminal directory to this program)
 
